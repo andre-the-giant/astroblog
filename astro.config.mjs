b309fbd077562ed129c7f229d8af42ac539d83e4
Andre The Giant <andrecollin.toronto@gmail.com>
2025-03-16 16:15:02 -0400

    robots.txt and sitemap ADDED

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    outDir: 'build', 
-    site:"https://andre-collin.3615toronto.ca",
-
-  });
+  outDir: 'build',
+  site:"https://andre-collin.3615toronto.ca",
+  integrations: [sitemap()]
+});
