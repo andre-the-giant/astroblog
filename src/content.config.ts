ffbbbeb7fa13789c9a4e0066ab631bc9e1cf7757
Andre The Giant <andrecollin.toronto@gmail.com>
2025-10-24 16:50:04 -0400

    updated workingtogether to use content collection. Added a testimony on homepage

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,17 +1,39 @@
+// src/content/config.ts
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
+
+// BLOG COLLECTION
 const blogCollection = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/pages/posts" }),
+  loader: glob({ pattern: '**/*.md', base: './src/pages/posts' }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
     pubDate: z.coerce.date(),
-    image: z.object({
-      url: z.string().url().or(z.string()).optional(),
-      alt: z.string().optional(),
-    }).optional(),
+    image: z
+      .object({
+        url: z.string().url().or(z.string()).optional(),
+        alt: z.string().optional(),
+      })
+      .optional(),
     tags: z.array(z.string()).optional(),
   }),
 });
 
-export const collections = { blog:blogCollection };
+// TESTIMONIES COLLECTION
+const testimoniesCollection = defineCollection({
+  loader: glob({ pattern: '**/*.md', base: './src/content/testimonies' }),
+  schema: z.object({
+    name: z.string(),
+    year: z.number().int().optional(), // for sorting by year
+    picture: z.string().optional(), // path to an image
+    excerpt: z.string().optional(),
+    company: z.string().optional(),
+    linkedin: z.string().url().optional(),
+    relationship: z.string().optional(),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+  testimonies: testimoniesCollection,
+};
